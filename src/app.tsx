8aea651635d08f279554e66855196935342fce71
Johnson Wang <ycwang.cs@gmail.com>
2024-10-05 14:04:53 +0800

    feat: :heavy_plus_sign: Setup PrimeReact

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,12 @@
+import { PrimeReactProvider } from "primereact/api";
+import { Chip } from "primereact/chip";
 import { useTranslation } from "react-i18next";
 
 export const App: React.FC = () => {
     const { t } = useTranslation();
-    return <>{t("helloWorld", { ns: ["common"] })}</>;
+    return (
+        <PrimeReactProvider>
+            <Chip label={t("helloWorld", { ns: ["common"] })} icon="pi pi-check" />
+        </PrimeReactProvider>
+    );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { App } from "@/app";
 import "@/locale/setup";
 
 import "normalize.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/lara-light-teal/theme.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
