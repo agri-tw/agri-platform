75274adafbe8cb8a74fa92ef24ae22156bec488d
Johnson Wang <ycwang.cs@gmail.com>
2024-10-05 20:13:37 +0800

    build: :building_construction: Build a Flask app in Firebase Cloud Functions and expose API /test, /test/<name>

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,23 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
-from firebase_functions import https_fn
+import flask
 from firebase_admin import initialize_app
+from firebase_functions import https_fn
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+app = flask.Flask(__name__)
+
+
+@app.get("/test")
+@app.get("/test/<name>")
+def test(name=None):
+    args = flask.request.args
+    resp_json = {"args": args, "name": name}
+    return resp_json, 200
+
+
+# Expose Flask app as a single Cloud Function:
+
+
+@https_fn.on_request()
+def agri_server(req: flask.Request) -> flask.Response:
+    with app.request_context(req.environ):
+        return app.full_dispatch_request()
