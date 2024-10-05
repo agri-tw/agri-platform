1a78acafa51aa7c40506940a0d14851fe02ca2f3
Johnson Wang <ycwang.cs@gmail.com>
2024-10-05 21:31:05 +0800

    feat: :sparkles: protected props for Layout

--- src/components/common/routes/layout.tsx
+++ src/components/common/routes/layout.tsx
@@ -1,19 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { useMemo } from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
 import { AppShell } from "@mantine/core";
 
+import { useIsLoggedInQuery } from "@/api/user";
 import { AuthRedirectResult, Navbar } from "@/components/common";
+import { url } from "@/constants/routes";
 
-export const Layout: React.FC = () => {
+interface LayoutProps {
+    /**
+     * Whether to prohibit unauthorized users from accessing the layout
+     */
+    protected?: boolean;
+}
+
+export const Layout: React.FC<LayoutProps> = (props) => {
+    const { data: isLoggedIn, isLoading: checkingIsLoggedIn } = useIsLoggedInQuery();
+
+    const authorized = useMemo(() => !props.protected || isLoggedIn, [props.protected, isLoggedIn]);
+    const unauthorized = useMemo(() => !checkingIsLoggedIn && !authorized, [checkingIsLoggedIn, authorized]);
+
     return (
-        <AppShell padding="md" header={{ height: 64 }}>
-            <AuthRedirectResult />
-            <AppShell.Header>
-                <Navbar />
-            </AppShell.Header>
-            <AppShell.Main>
-                <Outlet />
-            </AppShell.Main>
-        </AppShell>
+        <>
+            {unauthorized && <Navigate to={url.home} replace={true} />}
+            <AppShell padding="md" header={{ height: 64 }}>
+                <AuthRedirectResult />
+                <AppShell.Header>
+                    <Navbar />
+                </AppShell.Header>
+                <AppShell.Main>{authorized ? <Outlet /> : "Authenticating..."}</AppShell.Main>
+            </AppShell>
+        </>
     );
 };
