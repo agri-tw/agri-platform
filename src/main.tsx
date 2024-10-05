26214780ba14d8974d6d152f2f973c027e0c0180
Johnson Wang <ycwang.cs@gmail.com>
2024-10-05 13:55:32 +0800

    style: :lipstick: add normalize css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "@/app";
 import "@/locale/setup";
 
+import "normalize.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
