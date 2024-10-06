1f09bec60d0eda3265d8c78252f595ad465e699b
Johnson Wang <ycwang.cs@gmail.com>
2024-10-06 08:01:57 +0800

    feat: :globe_with_meridians: Add language for api /predict

--- src/api/advisor/predict.ts
+++ src/api/advisor/predict.ts
@@ -4,11 +4,11 @@ import { cloudFunctionApiSlice } from "../apiSlice";
 
 export const { usePredictMutation } = cloudFunctionApiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        predict: builder.mutation<Advice, { lat: number; long: number; crop: string }>({
-            query: ({ lat, long, crop }) => ({
+        predict: builder.mutation<Advice, { lat: number; long: number; crop: string; language: string }>({
+            query: ({ lat, long, crop, language }) => ({
                 url: "/predict",
                 method: "POST",
-                body: { lat, long, crop },
+                body: { lat, long, crop, language },
             }),
             invalidatesTags: ["predict"],
         }),
--- src/components/advisor/advisorSections/advisorInputSection/advisorInputSection.tsx
+++ src/components/advisor/advisorSections/advisorInputSection/advisorInputSection.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import { Button, Stack } from "@mantine/core";
 
 import { AddressInput, CropSelection } from "@/components/advisor";
+import { languageConfigs } from "@/locale/languageConfigs";
 import { fetchUserCoordinates } from "@/utils/user";
 
 import { usePosition } from "./usePosition";
 
 interface AdvisorInputSectionProps {
-    predict: (args: { crop: string; lat: number; long: number }) => void;
+    predict: (args: { crop: string; lat: number; long: number; language: string }) => void;
 }
 
 export const AdvisorInputSection: React.FC<AdvisorInputSectionProps> = (props) => {
+    const { i18n } = useTranslation();
     const [crop, setCrop] = useState<string>();
     const position = usePosition();
 
@@ -29,6 +32,7 @@ export const AdvisorInputSection: React.FC<AdvisorInputSectionProps> = (props) =
                 crop,
                 lat: position.coordinates.lat(),
                 long: position.coordinates.lng(),
+                language: languageConfigs[i18n.language as keyof typeof languageConfigs].name,
             });
         } else {
             console.error("Crop or coordinates are not selected yet");
