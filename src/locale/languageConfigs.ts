7abb152a166859eebb5a13605c1ea497ca4999d1
Johnson Wang <ycwang.cs@gmail.com>
2024-10-06 23:40:53 +0800

    add ja jp and ko kr languages

--- src/locale/languageConfigs.ts
+++ src/locale/languageConfigs.ts
@@ -1,6 +1,12 @@
 import resources_enus_advisor from "./en-us/advisor.json";
 import resources_enus_common from "./en-us/common.json";
 import resources_enus_home from "./en-us/home.json";
+import resources_jajp_advisor from "./ja-jp/advisor.json";
+import resources_jajp_common from "./ja-jp/common.json";
+import resources_jajp_home from "./ja-jp/home.json";
+import resources_kokr_advisor from "./ko-kr/advisor.json";
+import resources_kokr_common from "./ko-kr/common.json";
+import resources_kokr_home from "./ko-kr/home.json";
 import resources_zhtw_advisor from "./zh-tw/advisor.json";
 import resources_zhtw_common from "./zh-tw/common.json";
 import resources_zhtw_home from "./zh-tw/home.json";
@@ -20,6 +26,22 @@ export const languageConfigs = {
             advisor: resources_zhtw_advisor,
             common: resources_zhtw_common,
             home: resources_zhtw_home,
+        },
+    },
+    "ja-JP": {
+        name: "日本語",
+        resources: {
+            advisor: resources_jajp_advisor,
+            common: resources_jajp_common,
+            home: resources_jajp_home,
+        },
+    },
+    "ko-KR": {
+        name: "한국어",
+        resources: {
+            advisor: resources_kokr_advisor,
+            common: resources_kokr_common,
+            home: resources_kokr_home,
         },
     },
 };
