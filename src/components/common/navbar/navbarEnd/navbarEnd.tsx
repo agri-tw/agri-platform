93d3d08efbfe4f769c1cbcbe882768140ee5d40f
Johnson Wang <ycwang.cs@gmail.com>
2024-10-06 23:49:32 +0800

    add github action icon

--- src/components/common/navbar/navbarEnd/navbarEnd.tsx
+++ src/components/common/navbar/navbarEnd/navbarEnd.tsx
@@ -1,10 +1,19 @@
-import { Group } from "@mantine/core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ActionIcon, Group } from "@mantine/core";
 
 import { AvatarMenu } from "@/components/common";
 
 export const NavbarEnd: React.FC = () => {
     return (
         <Group>
+            <ActionIcon
+                variant="transparent"
+                color="dark"
+                onClick={() => window.open("https://github.com/agri-tw", "_blank", "noopener,noreferrer")}
+            >
+                <FontAwesomeIcon size="xl" icon={faGithub} />
+            </ActionIcon>
             <AvatarMenu />
         </Group>
     );
